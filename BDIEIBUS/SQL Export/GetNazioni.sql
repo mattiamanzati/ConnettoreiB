9400918b2464246d30d022d56e6de23075869bcd
Stefano Teodorani <teopost@gmail.com>
2014-03-21 18:18:50 +0100

    Aggiunto export nazioni

---
+++ BDIEIBUS/SQL Export/GetNazioni.sql
@@ -0,0 +1,12 @@
+﻿/* Da usare per il debug in management studio
+DECLARE @release varchar(200)
+SELECT @release = '1.0'
+*/
+
+SELECT 
+	tb_codstat, 
+	rtrim(ltrim(tb_desstat)) as tb_desstat
+FROM 
+	dbo.tabstat WITH (NOLOCK)
+ ORDER BY 
+	tb_desstat
